4f58dc124b3c7f4f4e28868d67b37a6d7f3d4c95
AnYoHwan <gombepoo@gmail.com>
2022-12-06 17:30:07 +0900

    히스토리01

--- main/views.py
+++ main/views.py
@@ -4,6 +4,9 @@ import pandas as pd
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .func import crawl
+from .models import *
+from django.utils.dateformat import DateFormat
+from datetime import datetime
 # Create your views here.
 
 def index(request):
@@ -15,7 +18,7 @@ def second(request):
   #stt변수에 POST방식으로 'stt' 값을 전달 받는다. GET과 POST를 알아야한다.
   login_session = request.session.get('login_session', '')
   stt = request.POST.get('stt','')
-  print("111", stt)  #이 프린트는 딜레이해결 완료 할때까지 잠시 대기입니다.
+  # print("111", stt)  #이 프린트는 딜레이해결 완료 할때까지 잠시 대기입니다.
   stt_edit = stt.replace(' ',"%")
   cursor = connection.cursor()
   recipeSql = "select recipe_nm from tb_recipe where recipe_nm like '"+ stt_edit + "' limit 1;"
@@ -24,7 +27,6 @@ def second(request):
   
   if recipeRst:
     recipe_nm = recipeRst[0][0]
-    print(recipe_nm)
     strSql = "select tb_irdent.GD_TYPE_CD, IRDNT_NM, GD_NM, GD_NUM, GD_ENT_NM "\
             + "from tb_recipe join tb_irdent on tb_recipe.RECIPE_NUM = tb_irdent.RECIPE_NUM join tb_gds on tb_irdent.GD_TYPE_CD = tb_gds.GD_TYPE_CD "\
             + "where RECIPE_NM like '"+ recipe_nm + "' order by rand();"
@@ -38,10 +40,12 @@ def second(request):
   else:
     irdent_dict = ""
     recipe_nm = stt
+
   return render(request, 'main/second.html',{"irdent_dict":irdent_dict, "login_session":login_session, "recipe_nm":recipe_nm})
 
 @csrf_exempt
 def third(request):
+  login_session = request.session.get('login_session', '')
   lon = "126.982732"
   lat = "37.488236"
   cursor = connection.cursor()
@@ -67,27 +71,42 @@ def third(request):
   
   irdent = request.POST.get("test_value")
   checked = request.POST.get('good')
+  recipe_name = request.POST.get('recipe')
   irdent = eval(irdent)
   checked = checked.split()
-
   df = pd.DataFrame(irdent).T
   df = df[df["gd_num"].isin(list(map(int,checked)))]
+  df3 = df[df["gd_num"].isin(list(map(int,checked)))]
+
+  if request.method == 'POST':
+    recipe_name =  request.POST.get('recipe')
+    today = DateFormat(datetime.now()).format('Ymd')
+    
+    TBsearch = ThSearch1(user_id=login_session, recipe_nm=recipe_name, ht_date=today)
+    TBsearch.save()
+
+  for row in df3.itertuples():
+    print(row[2],row[3])
+
+    TBsearch = ThSearch2(user_id=login_session, irdnt_nm=row[2], gd_nm=row[3])
+    TBsearch.save()
   
   good_col = df['gd_num']
   good = good_col.tolist()
   
-  print("api 동작")
-  cost = crawl(good, ent)
-  print("api 동작완료")
+  # print("api 동작")
+  # cost = crawl(good, ent)
+  # print("api 동작완료")
   
-  df2 = pd.DataFrame(cost).T
-  df2 = df2.reset_index()
-  df2 = df2.rename(columns={'index':'gd_num'})
+  # df2 = pd.DataFrame(cost).T
+  # df2 = df2.reset_index()
+  # df2 = df2.rename(columns={'index':'gd_num'})
 
-  merdf = pd.merge(df, df2, how='outer').fillna(0)
-  sum_df = merdf.sum().to_dict()
-  irdent_all = merdf.T.to_dict()
+  # merdf = pd.merge(df, df2, how='outer').fillna(0)
+  # sum_df = merdf.sum().to_dict()
+  # irdent_all = merdf.T.to_dict()
+  irdent_all = df.T.to_dict()
 
   dentJson = json.dumps(map_list, ensure_ascii=False)
 
-  return render(request, 'main/third.html',{ "ent_list":dentJson, 'ent':ent, 'sum_df':sum_df, 'irdent_all':irdent_all})
+  return render(request, 'main/third.html',{ "ent_list":dentJson, 'ent':ent, 'irdent_all':irdent_all, "login_session":login_session})#, 'sum_df':sum_df
